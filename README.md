b9ea59315ff4786558cccbd4256c7e248a9c9bcb
ASLant <77436463+KA-ASLant@users.noreply.github.com>
2022-11-10 11:15:23 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# 无
+<meta name="referrer" content="no-referrer" /> 
+
+# Note
+
+做一个简约实用学习笔记
+
+[博客地址](https://www.aslant.top)
+
+![](https://i0.hdslb.com/bfs/album/c4cf2c320618f7faec6241b8ac66ca70d6732b78.jpg)
+
---
+++ js/config.js
@@ -0,0 +1,15 @@
+export default {
+    // Gitee 配置
+    gitee: {
+        // 账号https://gitee.com/
+        owner: "aslant",
+        // 仓库名称
+        repo: "Note",
+        // 分支名
+        sha: "master"
+    },
+    // 后端地址
+    serverBase: "https://api.n0ts.cn/api/gitee",
+    // 主题配置
+    Themes: ["default-light", "default-dark"]
+};
